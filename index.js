022e3af76e1e5f745e582b7c4b38c0f33f0a18f9
Joel Rivera <joel.rivera7995@gmail.com>
2018-08-16 15:12:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
 }
+
 function nowServing(katzDeliLine){
   if(katzDeliLine.length === 0){
     return "There is nobody waiting to be served!";
@@ -9,6 +10,7 @@ function nowServing(katzDeliLine){
     return "Currently serving " + katzDeliLine.shift(0) + ".";
    }
  }
+ 
  function currentLine(katzDeliLine){
   if(katzDeliLine.length === 0){
   return "The line is currently empty.";
